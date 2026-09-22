24ce25800fa99c2e3a966ab5f13724f8dbfa126d
Yass1234y <wapoyassin2006@gmail.com>
2026-09-22 08:51:33 +0100

    Update Project

---
+++ data/applications/Bewerbung_Yassine_Bouzagaouy/1/saved_values.py
@@ -0,0 +1,5 @@
+
+old_salutation = 'e '
+old_person = 'x'
+old_email = 'info@kse-lippe.de'
+old_adresse_3 = 'Detmold'
---
+++ data/applications/Bewerbung_Yassine_Bouzagaouy/2/saved_values.py
@@ -0,0 +1,5 @@
+
+old_salutation = 'e '
+old_person = 'Frau Rau'
+old_email = 'info-margarethe@schwesternverband.de'
+old_adresse_3 = 'Lichtenau'
